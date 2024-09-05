da043301b4709849abc6b1b863a59d0644bf5f16
Mauro Navarro Sclosa <68697972+mnsclosa@users.noreply.github.com>
2024-09-05 10:34:26 -0300

    Ajustes no POV para negociar lote multiplo de 100 e incluido o teste escrito.

--- Pov/Pov.cpp
+++ Pov/Pov.cpp
@@ -3,28 +3,41 @@
 #include <iostream>
 
 /*
-* quantidadePOV: retorna a quantidade de ações a negociar quando uma determinada quantidade for negociada no mercado,
-*				 o exemplo é bem simples sem levar em consideração o preço limite para negociar.
+* funcaoRetornaQuantidade: retorna a quantidade de ações a negociar quando uma determinada quantidade for negociada no mercado,
+*						   respeitando sempre o multiplo de um lote.
+*						   o exemplo é bem simples sem levar em consideração o preço limite para negociar e outras variaveis.
 * 
-* porcentagem : Percentual a negociar quando o total negociado for atingido.
+* porcentagem : Porcentual a negociar quando o total negociado no mercado for atingido.
 * 
-* totalNegociado : total que será ultilizado para o calculo da quantiade
+* totalNegociado : total que será ultilizado para o calculo da quantidade
 * 
-* retorno: retorna o valor do calculo quando atingido o volume de negociação.
+* retorno: o valor do calculo quando atingido o volume de negociação sempre no tamanho de um lote minimo em caso de fracionado.
 * 
 */
 
-double quantidadePOV( double porcentagem,int totalNegociado )
+double funcaoRetornaQuantidade( double porcentagem,int totalNegociado )
 {
-	return totalNegociado * porcentagem; /* quantidade de ações que serão negociadas quando o volume for atingido */
+	int resto = 0;
+	int valoraNegociar = totalNegociado * porcentagem; /* quantidade de ações que serão negociadas quando o volume for atingido */
+
+	resto = (int)valoraNegociar % 100; /* especifico o tamanho de um lote */
+
+	/* garanto a negociação sempre de um lote multiplo de 100 ações */
+	if( resto )
+	{
+		/* garanto um lote inteiro */
+		valoraNegociar += ( 100.0 - (double)resto );
+	}
+	
+	return (double)valoraNegociar;
 }
 
 int main()
 {
-	double porcentagem = 0.1;  /* 10% do valor em decimal para facilitar o calculo */
+	double porcentagem = 0.10;  /* 10% do valor em decimal para facilitar o calculo */
 	int VolumeNegociado = 1000;   /* volume negociado para o calculo do POV */
 
-	std::cout << "Acoes a negociar quando atingir o volume esperado: " << quantidadePOV( porcentagem,VolumeNegociado ) << std::endl;
+	std::cout << "Acoes a negociar quando atingir o volume esperado: " << funcaoRetornaQuantidade( porcentagem,VolumeNegociado ) << std::endl;
 
 	return 0;
 }
